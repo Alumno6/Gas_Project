2230ea8f3d409b66156d272ed75758060b0bb5a8
Luis Arteaga Gomez <e.luisarteagagomez@go.ugr.es>
2024-06-18 17:53:01 +0000

    FALTA ALGUNOS RETOQUES

--- models/marts/fct_refueling.sql
+++ models/marts/fct_refueling.sql
@@ -2,16 +2,36 @@ with
 
 source as (
 
-    select * from {{ ref('stg_gas__repostajes') }}
+    select
+        {{ dbt_utils.star(from=ref('stg_gas__repostajes'),except=['timestamp'] ) }},
+        date(timestamp) as fecha,
+    from {{ ref('stg_gas__repostajes') }}
+
+),
+source_stations as (
+
+    select station_id, provincia_id 
+    from {{ ref('gasolineras_snapshot') }}
 
 ),
 
+
 renamed as (
 
-    select
-        *
-
-    from source
+    select 
+        R.transaction_id,
+        R.user_id,
+        car_id,
+        R.station_id,
+        fuel_id,
+        price,
+        liters,
+        metodo_pago,
+        fecha,
+        provincia_id
+    from source R 
+    join source_stations S 
+    on R.STATION_ID = S.STATION_ID
 
 )
 
--- models/marts/sum_fuel_company_total.sql
+++ models/marts/sum_fuel_company_total.sql
@@ -1,50 +1,73 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key = ['company_brand', 'month', 'year']
+    )
+}}
+
 with
+
 
 station_renamed as (
 
-    select * from {{ ref('Rename_station_company') }}
-
+    select 
+        {{ dbt_utils.star(from=ref('gasolineras_snapshot'),except=['company','_FIVETRAN_SYNCED','DBT_SCD_ID','DBT_UPDATED_AT'] ) }},
+        R.company_rename 
+    from {{ ref('Rename_station_company') }} R
+    join {{ ref('gasolineras_snapshot') }} G
+    on R.company = G.company
 ),
+
+refueling as (
+
+    select * from {{ ref('stg_gas__repostajes') }}
+
+    {% if is_incremental() %}
+
+        where   month(timestamp) >= (select coalesce(max(month), '1') from {{ this }}) and 
+                year(timestamp) >= (select coalesce(max(year), '1900') from {{ this }})
+
+    {% endif %}
+),
+
 
 province_station as (
 
     SELECT 
-        company_rename,
-        --provincia_id,
-        --month,
+        company_rename as company_brand,
+        provincia_id,
+        month,
         year,
         MAX(SUMA) as maximo
     FROM(
         SELECT
             company_rename,
-            --provincia_id,
-            --MONTH(timestamp) as month,
+            provincia_id,
+            MONTH(timestamp) as month,
             YEAR(timestamp) as year,
-            SUM(price*liters) as suma
-        FROM {{ ref('stg_gas__repostajes') }} R 
+            SUM(price*liters) as suma,
+            rank() over(partition by company_rename, MONTH(timestamp),YEAR(timestamp) order by SUM(price*liters) desc) as row_num
+        FROM refueling R 
         JOIN station_renamed S
+        on R.STATION_ID = S.STATION_ID
         GROUP BY 
                 company_rename,
-                --provincia_id,
-                --MONTH(timestamp),
+                provincia_id,
+                MONTH(timestamp),
                 YEAR(timestamp)
         )S
+    where row_num = 1
     GROUP BY 
         company_rename,
         provincia_id,
-        --month,
+        month,
         year
-),
-
-refueling as (
-
-    select * from {{ ref('fct_refueling') }}
 ),
 
 renamed as (
     
     select
-        S.company_rename,
+        P.company_brand,
         round(sum(price*liters),2) as total_amount_sales,
         count(transaction_id) as total_unit_sales,
         round(avg(price),2) as avg_liter_price,
@@ -56,10 +79,10 @@ renamed as (
     join station_renamed S 
     on R.STATION_ID = S.STATION_ID
     join province_station P
-    on S.company_rename = P.company_rename
+    on S.company_rename = P.company_brand
     
     group by 
-        S.company_rename, 
+        P.company_brand, 
         month(timestamp),
         year(timestamp),
         max_sold_province_id
